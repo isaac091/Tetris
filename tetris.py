8533d965c7c14bcd26f9382a14462b9ca94e41fa
isaac <isaacjs2@illinois.edu>
2020-07-19 01:48:47 -0500

    Change pieces to be 4 rectangles instead of a single polygon

--- tetris.py
+++ tetris.py
@@ -1,34 +1,79 @@
 import sys, pygame
 import copy
 import random
+import time
 pygame.init()
 
 black = 0, 0, 0
 screen = pygame.display.set_mode([750, 750])
 
 class Piece:
-    def __init__(self, pts, color):
-        self.pts = pts
+    def __init__(self, rects, color, shape):
+        self.rects = rects
         self.color = color
+        self.shape = shape
 
-all_pts = {
-    "s_pts": [[150, 25], [150, 50], [175, 50], [175, 75], [175, 100], [150, 100], [150, 75], [125, 75], [125, 50], [125, 25]],
-    "z_pts": [[150, 25], [175, 25], [175, 50], [175, 75], [150, 75], [150, 100], [125, 100], [125, 75], [125, 50], [150, 50]],
-    "j_pts": [[150, 25], [175, 25], [175, 50], [175, 75], [175, 100], [150, 100], [125, 100], [125, 75], [150, 75], [150, 50]],
-    "l_pts": [[150, 25], [150, 50], [150, 75], [175, 75], [175, 100], [150, 100], [125, 100], [125, 75], [125, 50], [125, 25]],
-    "t_pts": [[150, 25], [150, 50], [175, 50], [175, 75], [150, 75], [150, 100], [125, 100], [125, 75], [125, 50], [125, 25]],
-    "i_pts": [[150, 25], [175, 25], [200, 25], [200, 50], [175, 50], [150, 50], [125, 50], [100, 50], [100, 25], [125, 25]],
-    "o_pts": [[150, 25], [175, 25], [175, 50], [175, 75], [150, 75], [125, 75], [125, 50], [125, 25]]
+    def draw(self):
+        for rect in self.rects:
+            pygame.draw.rect(screen, self.color, rect)
+
+    def move(self, x, y):
+        for i in range(4):
+            self.rects[i] = self.rects[i].move(x, y)
+
+    def get_left(self):
+        left = 10000
+
+        for rect in self.rects:
+            if rect.left < left:
+                left = rect.left
+
+        return left
+
+    def get_right(self):
+        right = 0
+
+        for rect in self.rects:
+            if rect.left + rect.width > right:
+                right = rect.left + rect.width
+
+        return right
+
+    def get_top(self):
+        top = 10000
+
+        for rect in self.rects:
+            if rect.top < top:
+                top = rect.top
+
+        return top
+
+    def get_bottom(self):
+        bottom = 0
+
+        for rect in self.rects:
+            if rect.top + rect.height > bottom:
+                bottom = rect.top + rect.height
+
+        return bottom
+
+block_rects = {
+    "s_rects": [pygame.Rect(125, 25, 25, 25), pygame.Rect(125, 50, 25, 25), pygame.Rect(150, 50, 25, 25), pygame.Rect(150, 75, 25, 25)],
+    "z_rects": [pygame.Rect(150, 25, 25, 25), pygame.Rect(150, 50, 25, 25), pygame.Rect(125, 50, 25, 25), pygame.Rect(125, 75, 25, 25)],
+    "j_rects": [pygame.Rect(150, 25, 25, 25), pygame.Rect(150, 50, 25, 25), pygame.Rect(150, 75, 25, 25), pygame.Rect(125, 75, 25, 25)],
+    "l_rects": [pygame.Rect(125, 25, 25, 25), pygame.Rect(125, 50, 25, 25), pygame.Rect(125, 75, 25, 25), pygame.Rect(150, 75, 25, 25)],
+    "t_rects": [pygame.Rect(125, 25, 25, 25), pygame.Rect(125, 50, 25, 25), pygame.Rect(125, 75, 25, 25), pygame.Rect(150, 50, 25, 25)],
+    "i_rects": [pygame.Rect(100, 25, 25, 25), pygame.Rect(125, 25, 25, 25), pygame.Rect(150, 25, 25, 25), pygame.Rect(175, 25, 25, 25)],
+    "o_rects": [pygame.Rect(125, 25, 25, 25), pygame.Rect(125, 50, 25, 25), pygame.Rect(150, 25, 25, 25), pygame.Rect(150, 50, 25, 25)]
 }
 
-pieces = [Piece(copy.deepcopy(all_pts["s_pts"]), (255, 0, 0)),
-          Piece(copy.deepcopy(all_pts["z_pts"]), (255, 0, 0)),
-          Piece(copy.deepcopy(all_pts["j_pts"]), (255, 0, 0)),
-          Piece(copy.deepcopy(all_pts["l_pts"]), (255, 0, 0)),
-          Piece(copy.deepcopy(all_pts["t_pts"]), (255, 0, 0)),
-          Piece(copy.deepcopy(all_pts["i_pts"]), (255, 0, 0)),
-          Piece(copy.deepcopy(all_pts["o_pts"]), (255, 0, 0))]
-piece_names = ["s", "z", "j", "l", "t", "i", "o"]
+pieces = [Piece(copy.deepcopy(block_rects["s_rects"]), (0, 240, 0), "s"),
+          Piece(copy.deepcopy(block_rects["z_rects"]), (240, 0, 0), "z"),
+          Piece(copy.deepcopy(block_rects["j_rects"]), (0, 0, 240), "j"),
+          Piece(copy.deepcopy(block_rects["l_rects"]), (240, 160, 0), "l"),
+          Piece(copy.deepcopy(block_rects["t_rects"]), (160, 0, 240), "t"),
+          Piece(copy.deepcopy(block_rects["i_rects"]), (0, 240, 240), "i"),
+          Piece(copy.deepcopy(block_rects["o_rects"]), (240, 240, 0), "o")]
 
 def draw_grid():
     for i in range(1, 12):
@@ -36,42 +81,45 @@ def draw_grid():
     for i in range (1, 22):
         pygame.draw.line(screen, (255, 255, 255), [25, i * 25], [275, i * 25])
 
-def move_poly(pts, x, y):
-    for pt in pts:
-        pt[0] = pt[0] + x
-        pt[1] = pt[1] + y
-
-    return pts
-
+last_time_moved = time.time()
 new_piece = True
 curr_piece_index = 0
-curr_piece = pygame.draw.polygon(screen, pieces[curr_piece_index].color, pieces[curr_piece_index].pts)
+curr_piece = pieces[0]
 
 while 1:
     if new_piece:
         # reset start position for next piece of the same type
-        pieces[curr_piece_index] = Piece(copy.deepcopy(all_pts[piece_names[curr_piece_index] + "_pts"]), (255, 0, 0))
+        pieces[curr_piece_index].rects = copy.deepcopy(block_rects[pieces[curr_piece_index].shape + "_rects"])
 
         curr_piece_index = random.randint(0,6)
+        curr_piece = pieces[curr_piece_index]
         new_piece = False
+
+    if curr_piece.get_bottom() > 525:
+        new_piece = True;
+
+    if time.time() - last_time_moved >= .5:
+        last_time_moved = time.time()
+        curr_piece.move(0, 25)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w and curr_piece.top > 25:
-                pieces[curr_piece_index].pts = move_poly(pieces[curr_piece_index].pts, 0, -25)
-            elif event.key == pygame.K_a and curr_piece.left > 25:
-                pieces[curr_piece_index].pts = move_poly(pieces[curr_piece_index].pts, -25, 0)
-            elif event.key == pygame.K_s and curr_piece.top + curr_piece.height < 525:
-                pieces[curr_piece_index].pts = move_poly(pieces[curr_piece_index].pts, 0, 25)
-            elif event.key == pygame.K_d and curr_piece.left + curr_piece.width < 275:
-                pieces[curr_piece_index].pts = move_poly(pieces[curr_piece_index].pts, 25, 0)
+            if event.key == pygame.K_w and curr_piece.get_top() > 25:
+                curr_piece.move(0, -25)
+            elif event.key == pygame.K_a and curr_piece.get_left() > 25:
+                curr_piece.move(-25, 0)
+            elif event.key == pygame.K_s and curr_piece.get_bottom() < 525:
+                curr_piece.move(0, 25)
+            elif event.key == pygame.K_d and curr_piece.get_right() < 275:
+                curr_piece.move(25, 0)
             elif event.key == pygame.K_r:
                 new_piece = True;
 
     screen.fill(black)
 
-    curr_piece = pygame.draw.polygon(screen, pieces[curr_piece_index].color, pieces[curr_piece_index].pts)
+    curr_piece.draw()
 
     draw_grid()
 
